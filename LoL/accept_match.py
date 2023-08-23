ad9706e788cd3af1b53c0eaa8c353058bcbe6081
abidmuin <muin.739@gmail.com>
2023-08-24 01:22:06 +0600

    updated script

--- LoL/accept_match.py
+++ LoL/accept_match.py
@@ -8,17 +8,17 @@ print("Waiting for Matchmaking..........")
 check_interval = 1
 
 while True:
-    button_position = pyautogui.locateOnScreen('accept_button.png', confidence=0.55)
-    
+    button_position = pyautogui.locateOnScreen("accept_button.png", confidence=0.55)
+
     if button_position:
         button_center = pyautogui.center(button_position)
         pyautogui.click(button_center)
-        
+
         print("Match Found and Accepted.")
         print("GLHF on the Rift!")
-        
+
         break
     else:
         print("In Queue...!")
-    
+
     time.sleep(check_interval)
